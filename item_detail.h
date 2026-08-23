4e2710b4bddb09550617c1e5b7dbb8d677ed9858
johodges <Jonathan.Hodges@ul.org>
2026-08-23 05:25:41 -0400

    Add modern item details and equipment comparison

---
+++ item_detail.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_ITEM_DETAIL_H
+#define ELORIA_ITEM_DETAIL_H
+#include "platform.h"
+void item_detail_update(const Uint8 *data, int len);
+void display_item_detail(void);
+void close_item_detail(void);
+#endif
