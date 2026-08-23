affc30ee0c907a62aa153fb4aaac887770e8f76e
johodges <Jonathan.Hodges@ul.org>
2026-08-23 04:51:44 -0400

    Add dedicated Nymara Exchange client window

---
+++ marketplace.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_MARKETPLACE_H
+#define ELORIA_MARKETPLACE_H
+#include "platform.h"
+void marketplace_update(const Uint8 *data, int len);
+void display_marketplace(void);
+void close_marketplace(void);
+#endif
