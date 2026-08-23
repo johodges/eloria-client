a718e8dc83e81adf71a05f1fb68bd1714e0594a6
johodges <Jonathan.Hodges@ul.org>
2026-08-23 05:41:36 -0400

    Add persistent player mail inbox

---
+++ mail_window.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_MAIL_WINDOW_H
+#define ELORIA_MAIL_WINDOW_H
+#include "platform.h"
+void mail_window_update(const Uint8 *data, int len);
+void display_mail_window(void);
+void close_mail_window(void);
+#endif
