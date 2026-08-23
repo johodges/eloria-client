ac1e2c8de30efa25165b2bb5244e2c7896d5aaa3
johodges <jonathanhooges@gmail.com>
2026-08-23 11:01:13 -0400

    Add modern tutorial window header

---
+++ tutorial_window.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_TUTORIAL_WINDOW_H
+#define ELORIA_TUTORIAL_WINDOW_H
+#include <SDL.h>
+void tutorial_window_update(const Uint8 *data, int len);
+void display_tutorial_window(void);
+void close_tutorial_window(void);
+#endif
