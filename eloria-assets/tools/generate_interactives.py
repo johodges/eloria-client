1cc3041bf914f43b9017bf29802e4f26fc344c78
johodges <Jonathan.Hodges@ul.org>
2026-08-22 23:53:10 -0400

    Add original interactive props and combat effects

---
+++ eloria-assets/tools/generate_interactives.py
@@ -0,0 +1,9 @@
+sed: can't read eloria-assets/tools/generate_interactives.pynprintf: No such file or directory
+sed: can't read __FILE_1__\n: No such file or directory
+sed: can't read eloria-assets/tools/generate_effects.pynprintf: No such file or directory
+sed: can't read __FILE_2__\n: No such file or directory
+sed: can't read eloria-assets/tools/generate_bootstrap_pack.pynprintf: No such file or directory
+sed: can't read __FILE_3__\n: No such file or directory
+sed: can't read eloria-assets/README.mdnprintf: No such file or directory
+sed: can't read __FILE_4__\n: No such file or directory
+sed: can't read eloria-assets/provenance.jsonn: No such file or directory
