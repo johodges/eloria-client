b21dbd5c3fd24f914f1830aa2193eacc229dff42
johodges <Jonathan.Hodges@ul.org>
2026-08-23 05:37:36 -0400

    Add waypoint navigation HUD

---
+++ navigation_hud.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_NAVIGATION_HUD_H
+#define ELORIA_NAVIGATION_HUD_H
+#include "platform.h"
+void navigation_hud_update(const Uint8 *data, int len);
+void display_navigation_hud(void);
+void close_navigation_hud(void);
+#endif
