eb7d262b13a6240cf3c0bddd92fb53acdb1f6c55
johodges <Jonathan.Hodges@ul.org>
2026-08-23 05:02:46 -0400

    Add dedicated NPC merchant window

---
+++ merchant.h
@@ -0,0 +1,7 @@
+#ifndef ELORIA_MERCHANT_H
+#define ELORIA_MERCHANT_H
+#include "platform.h"
+void merchant_update(const Uint8 *data, int len);
+void display_merchant(void);
+void close_merchant(void);
+#endif
