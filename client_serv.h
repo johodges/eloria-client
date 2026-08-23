f53c202101349fefc34ced1191af845d8d186dc7
johodges <Jonathan.Hodges@ul.org>
2026-08-23 05:35:30 -0400

    Wire authoritative combat HUD protocol

--- client_serv.h
+++ client_serv.h
@@ -847,6 +847,7 @@ typedef enum
 #define ELORIA_QUEST_JOURNAL_STATE 224
 #define ELORIA_ITEM_DETAIL 225
 #define ELORIA_INVENTORY_STATE 226
+#define ELORIA_COMBAT_STATE 227
 
 #define UPGRADE_NEW_VERSION 240	// TODO: Consider combining all this into one packet followed by one byte (plus optional text)
 #define UPGRADE_TOO_OLD 241
